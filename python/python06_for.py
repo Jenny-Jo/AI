92805ce9640fd3c2cdd19f5ade3b6f7c4989cc18
Jenny-Jo <38550304+Jenny-Jo@users.noreply.github.com>
2020-05-22 10:09:43 +0900

    Update python06_for.py

--- python/python06_for.py
+++ python/python06_for.py
@@ -1,23 +1,24 @@
 
-###### 반복문 ######
+###### for 반복문 ######
 
 a = {'name':'Jo', 'phone':'010', 'birth':'0509'} #문자형도 됨
 
-for i in a.keys(): # 다음줄로 엔터치면 자동으로 들여쓰기 해서 for 문 안에 들어있음을 명시
-    print(i)       # 각 keys가 i에 들어가
-                   # i 정의는 이미 되어있고, i 대신 다른 것 넣어줘도 됨, 그러나 통상적으로 i 씀
+for i in a.keys():                               # 다음줄로 엔터치면 자동으로 들여쓰기 해서 for 문 안에 들어있음을 명시
+    print(i)                                     # 각 keys가 i에 들어가
+                                                 # i 정의는 이미 되어있고, i 대신 다른 것 넣어줘도 됨, 그러나 통상적으로 i 씀
                    
 
-# name
-# phone
-# birth
+                                                # name
+                                                # phone
+                                                # birth
 
+#----------------------------------------------------------------------------
 a = [1,2,3,4,5,6,7,8,9,10]
-for i in a: # a인자 갯수 10번만큼 돌려라/
+for i in a:                                     # a인자 갯수 10번만큼 돌려라/
     i = i*i
-    print(i)
-    print('Hello') # Hello를 for문 안에서 10번 같이 출력 
-print('Hello')     # 한번 출력/for문과 동등 
+    print(i)                                    
+    print('Hello')                              # Hello를 for문 안에서 10번 같이 출력 
+print('Hello')                                  # 한번 출력/for문과 동등 
 
 # 1
 # Hello
@@ -41,6 +42,8 @@ print('Hello')     # 한번 출력/for문과 동등
 # Hello
 # Hello
 
+#----------------------------------------------------------------------------
+
 for i in a:
     print(i)
 # 1
@@ -57,7 +60,7 @@ for i in a:
 ######## while문 #########
 
 '''
-while 조건문 :        #참일 동안 계속 돈다 ???
+while 조건문 :        #참 True인 동안 계속 돈다 ???
     수행할 문장
 '''
 
@@ -65,29 +68,29 @@ while 조건문 :        #참일 동안 계속 돈다 ???
 if 1 :
     print('True')
 else :
-    print('False')
-# True
+    print('False')                                  # True 
+                                                
  
 
 if 3 :
     print('True')
 else :
-    print('False')
-# Ture
+    print('False')                                  # Ture
+
 
 
 if 0 :
     print('True')
 else :
-    print('False')
-#False ?????
+    print('False')                                  #False ?????
+
 
 
 if -1:
     print('True')
 else :
-    print('False')
-#True
+    print('False')                                  #True
+
 
 '''
 비교연산자
@@ -96,21 +99,21 @@ else :
 
 '''
 
-# if a = 1: # a에다 1을 넣는다>입력한다라고 인식
-#     print('출력될까') #SyntaxError: invalid syntax
+# if a = 1:                                         # a에다 1을 넣는다 > 입력한다 라고 인식
+#     print('출력될까')                              # SyntaxError: invalid syntax
 
 
 a = 1
 if a == 1:
     print('출력될까')
-#출력될까
+                                                    # 출력될까
 
 money = 10000
 if money >= 30000 :
     print('스파게티정식')
 else:
     print('하겐다즈한통')
-#하겐다즈한통
+                                                    # 하겐다즈한통
 
 
 #### 조건연산자 ####
@@ -118,25 +121,25 @@ else:
 
 money = 20000
 card = 1
-if money >= 30000 or card ==1: #둘 중 하나만 만족시키면 됨
+if money >= 30000 or card ==1:                      # 둘 중 하나만 만족시키면 됨
     print('스파게티정식')
 else :
     print ('하겐다즈한통')
-#스파게티정식
+                                                    # 스파게티정식
 
 #--------------------------------------------
 score = [90, 25, 67,45, 80]
 number = 0
-for i in score : # i에 인수가 하나씩 들어간다 #i에 90들어가,
+for i in score :                                    # i에 인수가 하나씩 들어간다 #i에 90들어가,
     if i >= 60:
         print("경]합격[축")
         number = number + 1
 print ("합격인원:",number,"명")
 
-# 경]합격[축
-# 경]합격[축
-# 경]합격[축
-# 합격인원: 3 명
+                                                    # 경]합격[축
+                                                    # 경]합격[축
+                                                    # 경]합격[축
+                                                    # 합격인원: 3 명
 
 #------------------------------------------
 #break, continue// break를 더 많이 쓴다
@@ -146,17 +149,17 @@ print("===============break=======================")
 
 score = [90, 25, 67,45, 80]
 number = 0
-for i in score : # i에 인수가 하나씩 들어간다 #i에 90들어가,
+for i in score :                                    # i에 인수가 하나씩 들어간다 #i에 90들어가,
     if i < 30:
-        break # break 걸리면 제일 가까운 for문 중지시킴/25에 걸려서 멈춤
+        break                                        # break 걸리면 제일 가까운 for문 중지시킴/25에 걸려서 멈춤
     
     if i >= 60:
         print("경]합격[축")
         number = number + 1
 print ("합격인원:",number,"명")
 
-# 경]합격[축
-# 합격인원: 1 명
+                                                    # 경]합격[축
+                                                    # 합격인원: 1 명
 
 print("===============continue=======================")
 score = [90, 25, 67,45, 80]
@@ -170,7 +173,7 @@ for i in score :
         number = number + 1
 print ("합격인원:",number,"명")
 
-# 경]합격[축
-# 경]합격[축
-# 경]합격[축
-# 합격인원: 3 명
+                                                    # 경]합격[축
+                                                    # 경]합격[축
+                                                    # 경]합격[축
+                                                    # 합격인원: 3 명
