1a164e0f98672474415118b7d13e01edb08a952a
Jenny-Jo <38550304+Jenny-Jo@users.noreply.github.com>
2020-05-22 11:10:28 +0900

    Update python09_Func.py

--- python/python09_Func.py
+++ python/python09_Func.py
@@ -1,47 +1,49 @@
-def sum1(a, b): #sum이란 함수에 a, b 값의 변수를 넣겠다
-    return a + b #C+d 이렇게 없는거 쓰면 안됨
+### 함수 만들기 ###
+def sum1(a, b):             # sum이란 함수에 a, b 값의 변수를 넣겠다
+    return a + b            # c + d 이렇게 없는거 쓰면 안됨
 
 # print(sum(3, 4))
 
 a = 1
 b = 2
-c = sum1(a, b) #위치만 맞으면 됨
+c = sum1(a, b)              # 위치만 맞으면 됨
 
-print(c) #3
+print(c)                    # 3
 
-##곱,나눗셈,뺄셈 함수 만들기##
+## 곱,나눗셈,뺄셈 함수 만들기 ##
 # mul1, div1,sub1
 
+#곱셈
 def mul1(a, b) :
-    return a * b
+    return a * b        
 
 a = 1
 b = 2
 c = mul1(a, b)
-print(c)
+print(c)                    # 2
 
+#나눗셈
 
 def div1 (a, b) :
-    return a / b
-
+    return a / b            
 
 a = 1
 b = 2
-c = mul1(a, b)
-print('div:  ', c)
+c = div1(a, b)
+print('div:  ', c)         # div:   0.5
 
-
+#뺄셈
 def sub1 (a, b) :
     return a - b
 
 c = sub1(a, b)
-print('sub1 : ', c)
+print('sub1 : ', c)        # sub1 :  -1
 
 def sayYeah():
     return 'Hi'
 aaa = sayYeah()
 print(aaa)
-# Hi 
+                          # Hi  ???
 # #매개변수 (parameter) a, b가 없는 함수
 
 def sum1(a, b,c): 
@@ -51,4 +53,4 @@ a = 1
 b = 2
 c = 34
 d = sum1(a, b, c)
-print(d)
+print(d)                # 37
