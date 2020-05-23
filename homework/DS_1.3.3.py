e06425343ac6876a385ddd4928201249b6132a04
Jenny-Jo <38550304+Jenny-Jo@users.noreply.github.com>
2020-05-23 22:07:00 +0900

    Update DS_1.3.3.py

--- homework/DS_1.3.3.py
+++ homework/DS_1.3.3.py
@@ -9,8 +9,26 @@ Salary_by_tenure = defaultdict(list)
 for salary, tenure in salaries_and_teures :
   salary_by_tenure[tenure].append(salary)
   
-average_salary_by_tenure = {
-  tenure: sum(salaries) / len(salaries)
-  for tenure, salaries in salary_by_tenure.itesm()
+# average_salary_by_tenure = {
+#   tenure: sum(salaries) / len(salaries)
+#   for tenure, salaries in salary_by_tenure.items()
+
+def tenure_bucket(tenure): 
+  if tenure < 2:
+    return "less than two"
+  elif tenure < 5:
+    return "between two and five"
+  else:
+    return " more than five"
+
+salary_by_tenure_bucket = defaultdict(list)
+ 
+for salary, tenure in salaries_and_tenures:
+  bucket = tenure_bucket(tenure)
+  salary_by_tenure_bucket[bucket].append(salary)
+
+average_salary_by_bucket = {
+  tenure_bucket : sum(salaries)/ len(salaries)
+  for tenure_bucket, salaries in salary_by_tenure_bucket.items()
   
   
