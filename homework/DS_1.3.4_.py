e41624e89cc0a4e321eb7e638a9473d7acba6573
Jenny-Jo <38550304+Jenny-Jo@users.noreply.github.com>
2020-05-23 22:11:28 +0900

    Create DS_1.3.4_.py

---
+++ homework/DS_1.3.4_.py
@@ -0,0 +1,9 @@
+years_experience = (0.7,1.9,2.5,4.2, 6.0)
+
+def predict_paid_or_unpaid(years_experience):
+  if years_experience < 3.0
+    return "paid"
+  elif years_experience < 8.5
+    return "unpaid"
+  else:
+    return "paid"
