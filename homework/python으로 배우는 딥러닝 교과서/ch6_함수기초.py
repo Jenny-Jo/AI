f1eb6643911b6380fa02fe513355bf70f0377041
Jenny-Jo <coco7kiti@gmail.com>
2020-06-07 20:49:51 +0900

    .

--- homework/python으로 배우는 딥러닝 교과서/ch6_함수기초.py
+++ homework/python으로 배우는 딥러닝 교과서/ch6_함수기초.py
@@ -107,7 +107,7 @@ class MyProduct:
         self.name =name
         self.price = price
         self.stock = stock
-        self.saleds = 0
+        self.sales = 0
 
 # product1 = MyProduct("cake",500,20)
 # print(product1.stock)
@@ -138,7 +138,7 @@ class MyProduct:
         self.name =name
         self.price = price
         self.stock = stock
-        self.saleds = 0
+        self.sales = 0
 
     def summary(self):
         message = "called summary().\n name:" + self.name + \
@@ -155,4 +155,57 @@ class MyProduct:
 
 class MyProductSalesTax(MyProduct):
     def __init__(self, name, price, stock, tax_rate):
+        super().__init__(name, price, stock)
+        self.tax_rate = tax_rate
+    def get_name(self):
+        return self.name + "(소비세 포함)"
+    def get_price_with_tax(self):
+        return int(self.price*(1+self.tax_rate))
+    def summary(self):
+        message = 'called summary().\n name:' + self.get_name()+\
+                  '\n price:' + str(self.get_price_with_tax()+0) + \
+                  '\n stock:' + str(self.stock) + \
+                  '\n sales: '+ str(self.sales)
+        print(message)
+
+product_3 = MyProductSalesTax("phone", 30000, 100, 0.1)
+print(product_3.get_name())
+print(product_3.get_price_with_tax())
+product_3.summary()
+
+# 문자열 포맷 지정
+pai = 3.141592
+print("원주율은 %f" % pai)
+print("원주율은 %.2f" % pai)
+
+def bmi(h,w):
+    return w/h**2
+h = 1.6
+w = 52
+my_bmi = bmi(h,w)
+print("bmi는 %.4f입니다."%bmi(1.6, 52))
+
+# 연습문제
+def check_character(object, character):
+    return object.count(character)
+print(check_character([1,3,5,6,7,3,23,5,7,8,3], 3))
+print(check_character('a;sldkjfieujfija;slkdfj','d'))
+
+# 종합문제
+def binary_search(numbers, target_number):
+    low = 0
+    high = len(numbers)
+    while low <=high:
+        middle = (low+high)//2
+        if numbers[middle] == target_number:
+            print("{1}은 {0}째에 있습니다.".format(middle, target_number))
+            break
+        elif numbers[middle]< target_number:
+            low = middle + 1
+        else:
+            high = middle - 1
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+target_number = 11
+binary_search(numbers, target_number)
+    
         
